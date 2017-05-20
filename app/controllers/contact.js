a05065681d200f0501b7924dbd759bbfe41429e2
Claude Ayitey <clauditey@gmail.com>
2017-05-20 01:03:17 +0000

    just completed lesson 2 and its assignment. I'm surprised!.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -5,6 +5,15 @@ export default Ember.Controller.extend({
     emailIsValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
     textMessageReady: Ember.computed.gte('textMessage.length', 7),
 
-    isDisabled: Ember.computed.and('emailIsValid', 'textMessageReady')
+    isValid: Ember.computed.and('emailIsValid', 'textMessageReady'),
+    isNotValid: Ember.computed.not('isValid'),
 
+    actions: {
+        buttonAlert() {
+            alert(`Email: ${this.get('emailAddress')}`);
+            this.set('emailAddress', '');
+            this.set('textMessage', '');
+            this.set('responseMessage', `We got your message and we\'ll get in touch soon.`);
+        }
+    }
 });
